fd91e80c6df30889d5662c0dc6dd6a1812ffc1e7
horverno <horverno@gmail.com>
2022-07-25 09:02:57 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# pointcloud_to_grid
-This package converts sensor_msgs/PointCloud2 LIDAR data to nav_msgs/OccupancyGrid 2D map data based on intensity and / or height.
+# `pointcloud_to_grid` ROS package
+This package converts `sensor_msgs/PointCloud2` LIDAR data to `nav_msgs/OccupancyGrid` 2D map data based on intensity and / or height.
+![](doc/grid_map01.gif)
+
+## Build
+```
+cd ~/catkin_ws/src 
+git clone https://github.com/jkk-research/pointcloud_to_grid
+catkin build pointcloud_to_grid
+```
+Don't foget to `source ~/catkin_ws/devel/setup.bash`. *Note*: [`catkin tools`](https://catkin-tools.readthedocs.io/en/latest/verbs/catkin_build.html) is recommended, but `catkin_make` also works.
+
+
+## Features
+- Few dependencies (ROS and PCL mainly) [ROS installation](http://wiki.ros.org/ROS/Installation)
+- Simple as possible
+- Fast
+
+## Conventions & definitions
+![](doc/grid_map_conventions.svg)
+
+# Getting started
+
+Issue the following commands to start roscore, download and play sample data, and start the algorithm with visualization.
+
+In a **new terminal** start roscore:
+
+```
+roscore
+```
+
+In a **new terminal** go to your bag folder (e.g. `~/Downloads`):
+
+```
+cd ~/Downloads
+```
+
+Download a sample rosbag (~3,3 GB):
+
+```r
+wget https://laesze-my.sharepoint.com/:u:/g/personal/herno_o365_sze_hu/EYl_ahy5pgBBhNHt5ZkiBikBoy_j_x95E96rDtTsxueB_A?download=1 -O leaf-2021-04-23-campus.bag
+```
+
+Play rosbag:
+
+```r
+rosbag play -l ~/Downloads/leaf-2021-04-23-campus.bag
+```
+
+Start the algorithm in a **new terminal** :
+```r
+roslaunch pointcloud_to_grid demo.launch
+```
+
+Start the visualization in a **new terminal** :
+```r
+roslaunch pointcloud_to_grid rviz.launch
+```
+
+
+## Related solutions
+- [github.com/ANYbotics/grid_map](https://github.com/ANYbotics/grid_map) - This is a C++ library with ROS interface to manage two-dimensional grid maps with multiple data layers. 
+- [github.com/306327680/PointCloud-to-grid-map](https://github.com/306327680/PointCloud-to-grid-map) - A similar solution but instead PointCloud2 it uses PointCloud
+
+
+## Cite & paper
+
+If you use any of this code please consider citing TODO:
+
+```bibtex
+```
---
+++ src/pointcloud_to_grid_node.cpp
@@ -0,0 +1,105 @@
+#include <ros/ros.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+#include <pcl_ros/point_cloud.h>
+#include <pointcloud_to_grid/pointcloud_to_grid_core.hpp>
+#include <pointcloud_to_grid/MyParamsConfig.h>
+#include <dynamic_reconfigure/server.h>
+
+nav_msgs::OccupancyGridPtr intensity_grid(new nav_msgs::OccupancyGrid);
+nav_msgs::OccupancyGridPtr height_grid(new nav_msgs::OccupancyGrid);
+GridMap grid_map;
+ros::Publisher pub_igrid, pub_hgrid;
+ros::Subscriber sub_pc2;
+
+PointXY getIndex(double x, double y){
+  PointXY ret;
+  ret.x = int(fabs(x - grid_map.topleft_x) / grid_map.cell_size);
+  ret.y = int(fabs(y - grid_map.topleft_y) / grid_map.cell_size);
+  return ret;
+}
+
+void paramsCallback(my_dyn_rec::MyParamsConfig &config, uint32_t level)
+{
+  grid_map.cell_size = config.cell_size;
+  grid_map.position_x = config.position_x;
+  grid_map.position_y = config.position_y;
+  grid_map.cell_size = config.cell_size;
+  grid_map.length_x = config.length_x;
+  grid_map.length_y = config.length_y;
+  grid_map.cloud_in_topic = config.cloud_in_topic;
+  grid_map.intensity_factor = config.intensity_factor;
+  grid_map.height_factor = config.height_factor;
+  //grid_map.frame_out = config.frame_out;
+  grid_map.mapi_topic_name = config.mapi_topic_name;
+  grid_map.maph_topic_name = config.maph_topic_name;
+  grid_map.cloud_in_topic = config.cloud_in_topic;
+  grid_map.initGrid(intensity_grid);
+  grid_map.initGrid(height_grid);
+  grid_map.paramRefresh();
+}
+
+
+void pointcloudCallback(const pcl::PCLPointCloud2 &msg)
+{
+  pcl::PointCloud<pcl::PointXYZI> out_cloud;
+  pcl::fromPCLPointCloud2(msg, out_cloud);
+  // Initialize grid
+  grid_map.initGrid(intensity_grid);
+  grid_map.initGrid(height_grid);
+  // width*height/cell_size^2 eg width = 20m, height = 30m, cell_size data size = 6000
+  // or cell_num_x * cell_num_ys
+  // -128 127 int8[] data
+  std::vector<signed char> hpoints(grid_map.cell_num_x * grid_map.cell_num_y);
+  std::vector<signed char> ipoints(grid_map.cell_num_x * grid_map.cell_num_y);
+  for (auto p : hpoints){p = -128;}
+  for (auto p : ipoints){p = -128;}
+  //for (int i = 0; i < out_cloud.points.size(); ++i) // out_cloud.points[i].x instead of out_point.x
+  for (auto out_point : out_cloud)
+  {
+    if (out_point.x > 0.01 || out_point.x < -0.01){
+      if (out_point.x > grid_map.bottomright_x && out_point.x < grid_map.topleft_x)
+      {
+        if (out_point.y > grid_map.bottomright_y && out_point.y < grid_map.topleft_y)
+        {
+          PointXY cell = getIndex(out_point.x, out_point.y);
+          if (cell.x < grid_map.cell_num_x && cell.y < grid_map.cell_num_y){
+            ipoints[cell.y * grid_map.cell_num_x + cell.x] = out_point.intensity * grid_map.intensity_factor;
+            hpoints[cell.y * grid_map.cell_num_x + cell.x] = out_point.z * grid_map.height_factor;
+          }
+          else{
+            ROS_WARN_STREAM("Cell out of range: " << cell.x << " - " << grid_map.cell_num_x << " ||| " << cell.y << " - " << grid_map.cell_num_y);
+          }
+        }
+      }
+    }
+  }
+  intensity_grid->header.stamp = ros::Time::now();
+  intensity_grid->header.frame_id = msg.header.frame_id; // TODO
+  intensity_grid->info.map_load_time = ros::Time::now();
+  intensity_grid->data = ipoints;
+  height_grid->header.stamp = ros::Time::now();
+  height_grid->header.frame_id = msg.header.frame_id; // TODO
+  height_grid->info.map_load_time = ros::Time::now();  
+  height_grid->data = hpoints;
+  pub_igrid.publish(intensity_grid);
+  pub_hgrid.publish(height_grid);
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "pointcloud_to_grid_node");
+  dynamic_reconfigure::Server<my_dyn_rec::MyParamsConfig> server;
+  dynamic_reconfigure::Server<my_dyn_rec::MyParamsConfig>::CallbackType f;
+  f = boost::bind(&paramsCallback, _1, _2);
+  server.setCallback(f);
+  ros::NodeHandle nh;
+  pub_igrid = nh.advertise<nav_msgs::OccupancyGrid>(grid_map.mapi_topic_name, 1);
+  pub_hgrid = nh.advertise<nav_msgs::OccupancyGrid>(grid_map.maph_topic_name, 1);
+  sub_pc2 = nh.subscribe(grid_map.cloud_in_topic, 1, pointcloudCallback);
+  ros::spin();
+  return 0;
+}
