9b86b533bb9c16cb6a0a9de5aa272d6fb532f3cc
Mesics Matyas <mesicsmatyas@gmail.com>
2022-11-24 14:29:28 +0100

    Add Obstacle points with 100 weight

--- include/pointcloud_to_grid/pointcloud_to_grid_core.hpp
+++ include/pointcloud_to_grid/pointcloud_to_grid_core.hpp
@@ -1,6 +1,17 @@
 #pragma once
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+#include <pcl_ros/point_cloud.h>
+#include <pointcloud_to_grid/pointcloud_to_grid_core.hpp>
+#include <pointcloud_to_grid/point.h>
+#include <pointcloud_to_grid/MyParamsConfig.h>
+#include <dynamic_reconfigure/server.h>
+
+
 class PointXY{
 public: 
   int x;
@@ -38,19 +49,19 @@ class GridMap{
     std::vector<float> ranges {0.0, 0.0480580431416584, 0.04952912820025457, 0.05107271476990993, 0.05269363345060185, 0.05439712423301346, 0.05618887884892665, 0.05807508832254582, 0.06006249646573103, 0.06215846018173332, 0.06437101758608277, 0.06670896512456004, 0.06918194507246911, 0.0718005450435939, 0.07457641143041283, 0.07752237905008785, 0.08065261969712312, 0.08398281282093834, 0.08753034217607691, 0.09131452306247567, 0.09535686571760005, 0.09968138158636286, 0.10431494063602464, 0.10928768967611369, 0.11463354388430158, 0.12039076655489023, 0.1266026556430777, 0.1333183601959842, 0.14059385553413595, 0.14849311346929106, 0.15708951344942435, 0.1664675530375357, 0.17672493255767296, 0.1879751104675096, 0.20035045498449566, 0.21400615644938892, 0.2291251177806517, 0.24592411282444182, 0.2646616027219162, 0.2856477408593898, 0.30925729592893525, 0.33594650809441173, 0.3662753084830479, 0.4009369451742195, 0.440797978284289, 0.4869530100827344, 0.5408006990423111, 0.6041510737922202, 0.6793797969258417, 0.769654418585306, 0.8792737568385096, 1.014190021335894, 1.182835509100519, 1.3974753794571182, 1.6765073714116525, 2.0485501901036933, 2.560109231404816, 3.290908031115226, 4.387106404889913, 6.141023984061373, 9.21037993915209, 15.34909205824954, 30.695872599693494, 92.08299511357528};
 
 
-    void initGrid(nav_msgs::OccupancyGridPtr grid) {
-      grid->header.seq = 1;
-      grid->header.frame_id = GridMap::frame_out; // TODO
-      grid->info.origin.position.z = 0;
-      grid->info.origin.orientation.w = 0;
-      grid->info.origin.orientation.x = 0;
-      grid->info.origin.orientation.y = 0;
-      grid->info.origin.orientation.z = 1;
-      grid->info.origin.position.x = position_x + length_x / 2;
-      grid->info.origin.position.y = position_y + length_y / 2;
-      grid->info.width = length_x / cell_size;
-      grid->info.height = length_y /cell_size;
-      grid->info.resolution = cell_size;
+    void initGrid(nav_msgs::OccupancyGrid& grid) {
+      grid.header.seq = 1;
+      grid.header.frame_id = GridMap::frame_out; // TODO
+      grid.info.origin.position.z = 0;
+      grid.info.origin.orientation.w = 0;
+      grid.info.origin.orientation.x = 0;
+      grid.info.origin.orientation.y = 0;
+      grid.info.origin.orientation.z = 1;
+      grid.info.origin.position.x = position_x + length_x / 2;
+      grid.info.origin.position.y = position_y + length_y / 2;
+      grid.info.width = length_x / cell_size;
+      grid.info.height = length_y /cell_size;
+      grid.info.resolution = cell_size;
       // resolution/grid size [m/cell]
     }
     
@@ -97,4 +108,27 @@ class GridMap{
     }
 
     float get_Offset(int ring);
+};
+
+class ROSHandler
+{
+  public:
+
+    ros::Subscriber pc_road_sub,pc_obstacle_sub;
+    ros::Publisher occupancy_grid_pub;
+
+    pcl::PointCloud<ouster::Point>::ConstPtr obstacle_points;
+
+    dynamic_reconfigure::Server<my_dyn_rec::MyParamsConfig> dr_srv_;
+
+    GridMap grid_map;
+
+    void pointcloudCallback(const pcl::PointCloud<ouster::Point>::ConstPtr input_cloud);
+    void obstaclePointsCallback(const pcl::PointCloud<ouster::Point>::ConstPtr input_cloud);
+
+    PointXY getIndex(double x, double y);
+
+    ROSHandler(ros::NodeHandlePtr);
+    void paramsCallback(my_dyn_rec::MyParamsConfig &config, uint32_t level);
+
 };
--- src/pointcloud_to_grid_node.cpp
+++ src/pointcloud_to_grid_node.cpp
@@ -1,40 +1,16 @@
-#include <ros/ros.h>
-#include <nav_msgs/OccupancyGrid.h>
-#include <sensor_msgs/PointCloud.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl/PCLPointCloud2.h>
-#include <pcl/conversions.h>
-#include <pcl_ros/point_cloud.h>
 #include <pointcloud_to_grid/pointcloud_to_grid_core.hpp>
-#include <pointcloud_to_grid/point.h>
-#include <pointcloud_to_grid/MyParamsConfig.h>
-#include <dynamic_reconfigure/server.h>
 
-nav_msgs::OccupancyGridPtr intensity_grid(new nav_msgs::OccupancyGrid);
-nav_msgs::OccupancyGridPtr height_grid(new nav_msgs::OccupancyGrid);
-GridMap grid_map;
-ros::Publisher pub_igrid, pub_hgrid;
-ros::Subscriber sub_pc2;
 
-PointXY getIndex(double x, double y){
+PointXY ROSHandler::getIndex(double x, double y){
   PointXY ret;
   ret.x = int(fabs(x - grid_map.topleft_x) / grid_map.cell_size);
   ret.y = int(fabs(y - grid_map.topleft_y) / grid_map.cell_size);
   return ret;
 }
-
-float GridMap::get_Offset(int ring)
+void ROSHandler::paramsCallback(my_dyn_rec::MyParamsConfig &config, uint32_t level __attribute__((unused)))
 {
-  int actual_ring = 128 - ring;
-    
-  float offset_on_ring = ranges[actual_ring];
 
-  return offset_on_ring / cell_size;
-
-}
-
-void paramsCallback(my_dyn_rec::MyParamsConfig &config, uint32_t level)
-{
+  
   grid_map.cell_size = config.cell_size;
   grid_map.position_x = config.position_x;
   grid_map.position_y = config.position_y;
@@ -48,26 +24,52 @@ void paramsCallback(my_dyn_rec::MyParamsConfig &config, uint32_t level)
   grid_map.mapi_topic_name = config.mapi_topic_name;
   grid_map.maph_topic_name = config.maph_topic_name;
   grid_map.cloud_in_topic = config.cloud_in_topic;
-  grid_map.initGrid(intensity_grid);
-  grid_map.initGrid(height_grid);
+
   grid_map.paramRefresh();
 }
 
-
-void pointcloudCallback(const pcl::PointCloud<ouster::Point>::ConstPtr input_cloud)
+float GridMap::get_Offset(int ring)
 {
+  int actual_ring = 128 - ring;
+
+  float offset_on_ring = ranges[actual_ring];
+
+  return offset_on_ring / cell_size;
+}
+
+ROSHandler::ROSHandler(ros::NodeHandlePtr nh)
+{
+  dynamic_reconfigure::Server<my_dyn_rec::MyParamsConfig>::CallbackType f;
+
+  f = boost::bind(&ROSHandler::paramsCallback,this,_1,_2);
+
+  dr_srv_.setCallback(f);
+
+  pc_road_sub = nh->subscribe(grid_map.cloud_in_topic,1,&ROSHandler::pointcloudCallback,this);
+  pc_obstacle_sub = nh->subscribe("/obstacle_point",1,&ROSHandler::obstaclePointsCallback,this);
+
+  occupancy_grid_pub = nh->advertise<nav_msgs::OccupancyGrid>(grid_map.maph_topic_name, 1);
+
+}
+
+void ROSHandler::obstaclePointsCallback(const pcl::PointCloud<ouster::Point>::ConstPtr input_cloud)
+{
+  obstacle_points = input_cloud;
+}
+
+void ROSHandler::pointcloudCallback(const pcl::PointCloud<ouster::Point>::ConstPtr input_cloud)
+{
+  nav_msgs::OccupancyGrid occupancyGrid_msg;
   // Initialize grid
-  grid_map.initGrid(intensity_grid);
-  grid_map.initGrid(height_grid);
+  grid_map.initGrid(occupancyGrid_msg);
   // width*height/cell_size^2 eg width = 20m, height = 30m, cell_size data size = 6000
   // or cell_num_x * cell_num_ys
   // -128 127 int8[] data
-  std::vector<signed char> hpoints(grid_map.cell_num_x * grid_map.cell_num_y);
-  std::vector<signed char> ipoints(grid_map.cell_num_x * grid_map.cell_num_y);
-  // initialize grid vectors: -128
-  for (auto& p : hpoints){p = -128;}
-  for (auto& p : ipoints){p = -128;}
-  //for (int i = 0; i < out_cloud.points.size(); ++i) // out_cloud.points[i].x instead of out_point.x
+
+  std::vector<signed char> occ_points(grid_map.cell_num_x * grid_map.cell_num_y);
+  // initialize grid vectors: -1 as unknown
+  for (auto& p : occ_points){p = -1;}
+
   for (const auto& out_point : input_cloud->points)
   {
     if (out_point.x > 0.01 || out_point.x < -0.01){
@@ -85,50 +87,63 @@ void pointcloudCallback(const pcl::PointCloud<ouster::Point>::ConstPtr input_clo
             float y_off = (out_point.y / out_point.x) * x_off;
             PointXY off_setted_cell = getIndex(x_off,y_off);
             if (off_setted_cell.x < grid_map.cell_num_x && off_setted_cell.y < grid_map.cell_num_y){
-              ipoints[off_setted_cell.y * grid_map.cell_num_x + off_setted_cell.x ] = out_point.intensity * grid_map.intensity_factor;
-              hpoints[off_setted_cell.y * grid_map.cell_num_x + off_setted_cell.x ] = out_point.z * grid_map.height_factor;
-            }
-            else{
-              ROS_WARN_STREAM("Cell out of range: " << cell.x << " - " << grid_map.cell_num_x << " ||| " << cell.y << " - " << grid_map.cell_num_y);
+              occ_points[off_setted_cell.y * grid_map.cell_num_x + off_setted_cell.x ] = out_point.intensity * grid_map.intensity_factor;
             }
           }
           if(longitudinal_offset < 1)
           {
             if (cell.x < grid_map.cell_num_x && cell.y < grid_map.cell_num_y){
-              ipoints[cell.y * grid_map.cell_num_x + cell.x] = out_point.intensity * grid_map.intensity_factor;
-              hpoints[cell.y * grid_map.cell_num_x + cell.x] = out_point.z * grid_map.height_factor;
-            }
-            else{
-              ROS_WARN_STREAM("Cell out of range: " << cell.x << " - " << grid_map.cell_num_x << " ||| " << cell.y << " - " << grid_map.cell_num_y);
+              occ_points[cell.y * grid_map.cell_num_x + cell.x] = out_point.intensity * grid_map.intensity_factor;
             }
           }
         }
       }
     }
   }
-  intensity_grid->header.stamp = ros::Time::now();
-  intensity_grid->header.frame_id = input_cloud->header.frame_id; // TODO
-  intensity_grid->info.map_load_time = ros::Time::now();
-  intensity_grid->data = ipoints;
-  height_grid->header.stamp = ros::Time::now();
-  height_grid->header.frame_id = input_cloud->header.frame_id; // TODO
-  height_grid->info.map_load_time = ros::Time::now();  
-  height_grid->data = hpoints;
-  pub_igrid.publish(intensity_grid);
-  pub_hgrid.publish(height_grid);
+
+  for (const auto& out_point : obstacle_points->points)
+  {
+    if (out_point.x > 0.01 || out_point.x < -0.01)
+    {
+      if (out_point.x > grid_map.bottomright_x && out_point.x < grid_map.topleft_x)
+      {
+        if (out_point.y > grid_map.bottomright_y && out_point.y < grid_map.topleft_y)
+        {
+          PointXY cell = getIndex(out_point.x, out_point.y);
+          
+          if (cell.x < grid_map.cell_num_x && cell.y < grid_map.cell_num_y){
+            occ_points[cell.y * grid_map.cell_num_x + cell.x ] = 100;
+          }
+        }
+      }
+    }
+  }
+  
+
+  for(float i = 0;i > -4;i -= grid_map.cell_size)
+  {
+    for(float j = -1.5;j < 1.5;j += grid_map.cell_size)
+    {
+      PointXY cell = getIndex(i,j);
+      occ_points[cell.y * grid_map.cell_num_x + cell.x ] = 0;
+    }
+  }
+
+  occupancyGrid_msg.header.stamp = ros::Time::now();
+  occupancyGrid_msg.header.frame_id = input_cloud->header.frame_id; // TODO
+  occupancyGrid_msg.info.map_load_time = ros::Time::now();
+  occupancyGrid_msg.data = occ_points;
+  occupancy_grid_pub.publish(occupancyGrid_msg);
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "pointcloud_to_grid_node");
-  dynamic_reconfigure::Server<my_dyn_rec::MyParamsConfig> server;
-  dynamic_reconfigure::Server<my_dyn_rec::MyParamsConfig>::CallbackType f;
-  f = boost::bind(&paramsCallback, _1, _2);
-  server.setCallback(f);
-  ros::NodeHandle nh;
-  pub_igrid = nh.advertise<nav_msgs::OccupancyGrid>(grid_map.mapi_topic_name, 1);
-  pub_hgrid = nh.advertise<nav_msgs::OccupancyGrid>(grid_map.maph_topic_name, 1);
-  sub_pc2 = nh.subscribe(grid_map.cloud_in_topic, 1, pointcloudCallback);
+
+  ros::NodeHandlePtr nh = boost::make_shared<ros::NodeHandle>();
+
+  ROSHandler rosHandler(nh);
+
   ros::spin();
   return 0;
 }
