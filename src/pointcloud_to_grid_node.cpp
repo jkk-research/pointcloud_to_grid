ef1d34743f3cb64d2c1f0ead0dee6fcc9ef0c26b
Mesics Matyas <mesicsmatyas@gmail.com>
2022-12-04 21:48:00 +0100

    Add beam based projection

--- src/pointcloud_to_grid_node.cpp
+++ src/pointcloud_to_grid_node.cpp
@@ -32,9 +32,7 @@ float dist_from_point(int x,int y)
 }
 
 void ROSHandler::paramsCallback(my_dyn_rec::MyParamsConfig &config, uint32_t level __attribute__((unused)))
-{
-
-  
+{  
   grid_map.cell_size = config.cell_size;
   grid_map.position_x = config.position_x;
   grid_map.position_y = config.position_y;
@@ -48,6 +46,8 @@ void ROSHandler::paramsCallback(my_dyn_rec::MyParamsConfig &config, uint32_t lev
   grid_map.mapi_topic_name = config.mapi_topic_name;
   grid_map.maph_topic_name = config.maph_topic_name;
   grid_map.cloud_in_topic = config.cloud_in_topic;
+
+  this->config = config;
 
   grid_map.paramRefresh();
 }
@@ -79,6 +79,53 @@ ROSHandler::ROSHandler(ros::NodeHandlePtr nh)
   occupancy_grid_pub = nh->advertise<nav_msgs::OccupancyGrid>(grid_map.maph_topic_name, 1);
 
 }
+void ROSHandler::getBeams(auto &beam_list,double beam_num_,const pcl::PointCloud<ouster::Point>::ConstPtr input_cloud)
+{
+  const double beam_angle_resolution = 2.0 * M_PI / (double)beam_num_;
+
+  for(const auto &p : input_cloud->points)
+  {
+    const double distance = sqrt(p.x * p.x + p.y * p.y);
+    const double direction = atan2(p.y, p.x);
+    const int beam_index = (direction + M_PI) / beam_angle_resolution;
+    auto range_index = 2;
+    for(auto i = 1;i < grid_map.ranges.size();i++)
+    {
+      if(grid_map.ranges[i-1] < distance && distance < grid_map.ranges[i])
+      {
+        if(0 <= beam_index && beam_index < beam_num_)
+          beam_list[beam_index][i] = true;
+      }
+    }
+  }
+}
+void ROSHandler::set_map_cells_in_grid(const auto &beam_list, const std::vector<bool>& obstacle_indices, std::vector<signed char> &map,double beam_num_,double resolution_)
+{
+  const double beam_angle_resolution = 2.0 * M_PI / (double)beam_num_;
+  for(int i = 0;i < beam_num_; i++)
+  {
+    double direction = i * beam_angle_resolution - M_PI;
+    direction = atan2(sin(direction), cos(direction));
+    const double c = cos(direction);
+    const double s = sin(direction);
+
+    for(int r = 0; r < beam_list[i].size()-1 && r < grid_map.ranges.size()-1;r++)
+    {
+      if(beam_list[i][r] == true)
+      {
+        for(double range = grid_map.ranges[r-1]; range < grid_map.ranges[r+1]; range += resolution_)
+        {
+          const double x = range * c;
+          const double y = range * s;
+          const int index = getIndex(x, y);
+
+          if(index < map.size())
+            map[index] = 0;
+        }
+      }
+    }
+  }
+}
 
 void ROSHandler::obstaclePointsCallback(const pcl::PointCloud<ouster::Point>::ConstPtr input_cloud)
 {
@@ -98,40 +145,49 @@ void ROSHandler::pointcloudCallback(const pcl::PointCloud<ouster::Point>::ConstP
   // initialize grid vectors: -1 as unknown
   for (auto& p : occ_points){p = -1;}
 
-  for (const auto& out_point : input_cloud->points)
-  {
-    int longitudinal_offset = grid_map.get_Offset(out_point.ring);
+  // for (const auto& out_point : input_cloud->points)
+  // {
+  //   int longitudinal_offset = grid_map.get_Offset(out_point.ring);
 
-    std::vector<int> indexes_to_fill = getOffset_indexes(longitudinal_offset,out_point);
-    for(const auto& index : indexes_to_fill)
-    {
-      if (index < occ_points.size()){
-        occ_points[index] = out_point.intensity * grid_map.intensity_factor;
-      }
-    }
+  //   std::vector<int> indexes_to_fill = getOffset_indexes(longitudinal_offset,out_point);
+  //   for(const auto& index : indexes_to_fill)
+  //   {
+  //     if (index < occ_points.size()){
+  //       occ_points[index] = out_point.intensity * grid_map.intensity_factor;
+  //     }
+  //   }
 
-  }
+  // }
 
-  for (const auto& out_point : obstacle_points->points)
-  {
-    int index = getIndex(out_point.x, out_point.y);
+  // for (const auto& out_point : obstacle_points->points)
+  // {
+  //   int index = getIndex(out_point.x, out_point.y);
     
-    if (index < occ_points.size())
-      occ_points[index] = 100;
-  }
+  //   if (index < occ_points.size())
+  //     occ_points[index] = 100;
+  // }
   
 
-  for(float i = 0;i > -5;i -= grid_map.cell_size)
-  {
-    for(float j = -1.5;j < 1.5;j += grid_map.cell_size)
-    {
-      PointXY cell = getIndex(i,j);
-      occ_points[cell.y * grid_map.cell_num_x + cell.x ] = 0;
-    }
-  }
+  // for(float i = 0;i > -5;i -= grid_map.cell_size)
+  // {
+  //   for(float j = -1.5;j < 1.5;j += grid_map.cell_size)
+  //   {
+  //     int index = getIndex(i,j);
+  //     if (index < occ_points.size())
+  //       occ_points[index] = 0;
+  //   }
+  // }
+
+
+  double beam_num_ = config.beam_number;
+  std::vector<std::vector<bool>> beam_list(beam_num_, std::vector<bool>(grid_map.ranges.size(), false));
+  std::vector<bool> obstacle_indices;
+
+  getBeams(beam_list,beam_num_,input_cloud);
+  set_map_cells_in_grid(beam_list,obstacle_indices,occ_points,beam_num_,grid_map.cell_size/2);
 
   occupancyGrid_msg.header.stamp = ros::Time::now();
-  occupancyGrid_msg.header.frame_id = input_cloud->header.frame_id; // TODO
+  occupancyGrid_msg.header.frame_id = input_cloud->header.frame_id;
   occupancyGrid_msg.info.map_load_time = ros::Time::now();
   occupancyGrid_msg.data = occ_points;
   occupancy_grid_pub.publish(occupancyGrid_msg);
